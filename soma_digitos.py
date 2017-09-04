165afe68f469b241a12ce99858fa9276b31f8eb1
Renata Muylaert <renatamuy@gmail.com>
2017-09-04 16:41:58 -0300

    add spaces

--- soma_digitos.py
+++ soma_digitos.py
@@ -1,13 +1,11 @@
 n = int(input("Digite um número inteiro: "))
 
-#sempre zerar o objeto armazenador em while e criar o contador
-
-soma= 0
+soma = 0
 
 while n > 0: 
-        atual= n%10
-        soma= atual+ soma
-        n= n//10
+        atual = n%10
+        soma = atual+ soma
+        n = n//10
 
 print("A soma dos dígitos é :", soma)
 
