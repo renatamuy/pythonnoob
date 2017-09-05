43ed150977553bc76df21b9c0323c7b2485fbbab
Renata Muylaert <renatamuy@gmail.com>
2017-09-05 10:14:29 -0300

    week 5

---
+++ funcao_maximo.py
@@ -0,0 +1,8 @@
+def maximo(n, i):
+    #if i == int and n == int:
+        if n > i:
+            return(n)
+        else:
+            return(i)
+    #else:
+            print("Sorry")
---
+++ vogal.py
@@ -0,0 +1,8 @@
+def vogal(c):
+    if type(c) == str:
+        if c == "a" or c == "e" or c == "i" or c == "o" or c == "u" or c == "A" or c == "E" or c == "I" or c == "O" or c == "U":
+            return c == "a" or c == "e" or c == "i" or c == "o" or c == "u" or c == "A" or c == "E" or c == "I" or c == "O" or c == "U"
+        else:
+            return c == "a" or c == "e" or c == "i" or c == "o" or c == "u" or c == "A" or c == "E" or c == "I" or c == "O" or c == "U"
+    else:
+        print("Sorry")
