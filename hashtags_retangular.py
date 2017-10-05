5c0d84285b9df340ef90e810d14e421a4099bc59
Renata Muylaert <renatamuy@gmail.com>
2017-10-05 16:02:51 -0300

    Add files via upload

---
+++ hashtags_retangular.py
@@ -0,0 +1,51 @@
+largurareal = int(input("digite a largura: "))
+alturareal = int(input("digite a altura: "))
+
+
+#Assim digita uma coluna com 10
+
+#while altura <= alturareal:
+#    while largura <= largurareal:
+#        print("#", end = "")
+#        altura = altura + 1
+       
+ #       print() #nl new line
+ #       largura = largura + 1
+
+#Não precisa definir x antes
+
+x= 1
+altura = 1
+largura = 1
+
+
+#for x in range(x, alturareal):
+#while altura <= alturareal:
+#        while largura <= largurareal:
+#            print("#", end = "")         
+#            largura = largura + 1       
+#        print() #nl new line faz a sequencia para 1 linha
+#        print("#", end = "")
+#        altura = altura + 1
+ 
+x= 0
+altura = 1
+largura = 1
+
+
+for altura in range( x, alturareal):
+        print()
+        largura= 1
+        while largura <= largurareal:
+            print("#", end = "")         
+            largura = largura + 1       
+        #print() #nl new line faz a sequencia para 1 linha
+        #print("#", end = "")
+        #altura = altura + 1        
+    
+    
+
+
+
+##################################
+
