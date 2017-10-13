58c1d249855b18b0f41c0460a72b54f82d12432c
Renata Muylaert <renatamuy@gmail.com>
2017-10-13 14:31:14 -0300

    Add files via upload

---
+++ list_dependency.py
@@ -0,0 +1,17 @@
+'''List dependency'''
+
+origlist =[40, 50 , 60, 70]
+
+newlist = [origlist] * 3
+
+origlist[1] = 99
+
+newlist
+
+
+'''Pay attention on lista3'''
+
+lista1 = ["carro", "barco"]
+lista2 = [lista1] * 3
+lista3 = lista1 * 3
+lista1[1] = "metrô"
